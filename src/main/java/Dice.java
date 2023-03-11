556884dd4a58518f1d7679c0578a645269fc8d0f
Seth <seth@Zipcoders-MBP.home>
2023-03-11 11:22:26 -0500

    tested then wrote dice and bins

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,24 @@
+import java.util.Random;
+
 public class Dice {
 
+    private int diceNum;
 
+
+    public Dice(int i) {
+        this.diceNum = i;
+    }
+
+    public int tossAndSum() {
+        int total = 0;
+        Random rand = new Random(1);
+        for(int i = 0; i<getDiceNum(); i++){
+            total+=rand.nextInt(5)+1;
+        }
+        return total;
+    }
+
+    public int getDiceNum() {
+        return this.diceNum;
+    }
 }
---
+++ src/test/java/BinsTest.java
@@ -0,0 +1,28 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BinsTest {
+
+    @Test
+    public void testBinsConstructor() {
+        int expected = 10;
+
+        Bins bin = new Bins(2,12);
+
+        int actual = bin.getSize();
+
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetBin() {
+        int expected = 1;
+        Bins bin = new Bins(2,12);
+        bin.increment(10);
+
+        int actual = bin.getBin(10);
+
+        Assert.assertEquals(expected,actual);
+    }
+}
