e0c7e13eb1a67d9352fe1f643af09c3e230e578e
Nina <nina.emiko.mcnair@gmail.com>
2023-03-12 21:55:15 -0400

    Completed base of game

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,21 @@
 public class Dice {
 
+    public final int dice;
 
+    public Dice (int dice) {
+        this.dice = dice;
+    }
+
+    public static void rollDice(int num) {
+        int currentRoll = tossAndSum(num);
+        Bins.addToBin(currentRoll);
+        Bins.addToResult(currentRoll);
+    }
+
+    public static int tossAndSum(int num) {
+        double roll = Math.random() * 6 * num;
+        int rollInt = (int)Math.round(roll);
+//        System.out.println(rollInt);
+        return rollInt;
+    }
 }
--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
 public class Simulation {
 
+    public static Scanner scanner = new Scanner(System.in);
+    public static int numOfDice;
+    public static int numOfThrows;
 
+    public Simulation(int numOfDice, int numOfThrows){
+        this.numOfDice = numOfDice;
+        this.numOfThrows = numOfThrows;
+    }
+
+    public static void main(String[] args) {
+        Bins bin = new Bins();
+        System.out.println("How many dice would you like to roll?");
+        int numOfDice = scanner.nextInt();
+        System.out.println("How many times would you like to roll them?");
+        int numOfThrows = scanner.nextInt();
+
+        bin.createBin(numOfDice);
+        Simulation sim = new Simulation(numOfDice, numOfThrows);
+        runSimulation(sim);
+        printResults();
+    }
+
+    public static void runSimulation(Simulation sim) {
+        Dice dice = new Dice(sim.numOfDice);
+        for (int i = 0; i < sim.numOfThrows; i++) {
+            dice.rollDice(sim.numOfDice);
+        }
+    }
+
+    public static void printResults() {
+        //System.out.println("Total: " + Bins.runningTotal);
+        Bins.printMapResults();
+    }
 
 }
