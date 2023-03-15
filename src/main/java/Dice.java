e5b41673aa7bee9767d2ceb803451928ab8ca11b
adams <stilwelladam8@gmail.com>
2023-03-15 17:17:19 -0400

    Built everything but the data storage.

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,19 +1,18 @@
 import java.util.Random;
 
-public class Dice extends Bins {
+public class Dice {
+    int number;
+    int sides;
 
-    public void Roll(Integer numberOfDice, Integer numberOfRolls) {
-        Random rand = new Random();
-        StringBuilder sb = new StringBuilder();
-        int count = 1;
-        for(int i = 0; i<numberOfRolls; i++){
-            for(int j = 0; j<numberOfDice; j++){
-                sb.setLength(0);
-                sb.append("Roll #" + count);
-                map.put(sb.toString() ,rand.nextInt(11)+2);
-                count++;
-            }
-        }
+    public  Dice(){
+        number = 1;
+        sides = 6;
     }
+
+    public Dice(Integer numberOfDice, Integer numberOfSides){
+        this.number = numberOfDice;
+        this.sides = numberOfSides;
+    }
+
 }
 
--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,26 +1,29 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Simulation {
-    Dice dice = new Dice();
     Bins bins = new Bins();
     private Integer numberOfDice;
     private Integer numberOfRolls;
+    private Integer numberOfSides;
 
     Scanner in = new Scanner(System.in);
 
     public static void main(String[] args){
-
+        Simulation sim = new Simulation();
+        sim.Roll(sim.numberOfRolls);
     }//end of main
 
     public Simulation() {
         numberOfDice = 2;
-        numberOfRolls = 2;
+        numberOfRolls = 1000;
+        numberOfSides = 6;
     }
 
     public Simulation(Integer numberOfDice, Integer numberOfRolls){
         setNumberOfDice(numberOfDice);
+        setNumberOfSides(numberOfSides);
         setNumberOfRolls(numberOfRolls);
-        dice.Roll(numberOfDice, numberOfRolls);
     }
 
     public void setNumberOfDice(Integer numberOfDice){
@@ -30,6 +33,9 @@ public class Simulation {
     public void setNumberOfRolls(Integer numberOfRolls){
         this.numberOfRolls = numberOfRolls;
     }
+    public void setNumberOfSides(Integer numberOfSides){
+        this.numberOfSides = numberOfSides;
+    }
 
     public Integer getNumberOfDice(){
         return numberOfDice;
@@ -38,7 +44,24 @@ public class Simulation {
     public Integer getNumberOfRolls(){
         return numberOfRolls;
     }
+    public Integer getNumberOfSides(){
+        return numberOfSides;
+    }
 
+    public void Roll(Integer numberOfRolls) {
+        Integer sum = 0;
+        Random rand = new Random();
+        Dice dice = new Dice(numberOfDice, numberOfSides);
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i<numberOfRolls; i++){
+            sum = 0;
+            for(int j = 0; j<dice.number; j++){
+                sum += rand.nextInt(dice.sides)+1;
+            }
+            //store sum from number of rolls here
+            System.out.println(sum);
+        }
+    }
 
 
 }
