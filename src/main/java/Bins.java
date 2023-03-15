9ee8ac672c3d683b9b598b7676243880dca7ae4b
Jake Lawhorne <jake.lawhorne@gmail.com>
2023-03-14 22:04:43 -0400

    Lab done, doing tests now. Something happened to my repository and I had to refork it and copy and paste my code

--- src/main/java/Bins.java
+++ src/main/java/Bins.java
@@ -1,4 +1,29 @@
+import java.util.LinkedHashMap;
 
 public class Bins {
+    Dice die = new Dice(2);
 
+    public LinkedHashMap<Integer , Integer> storage = new LinkedHashMap<>();
+
+
+    public void setResultRange(Integer start, Integer end){
+        for(int i = start; i <= end; i++){
+            storage.putIfAbsent(i, 0);
+        }
+    }
+
+    public void getResultRange(Integer start, Integer end){
+        for(int i = start; i < end; i++){
+            storage.get(i);
+        }
+    }
+    public Integer getResultAtBin(Integer binNumber){
+        return storage.get(binNumber);
+    }
+
+    public void incrementBinValue(Integer diceValue){
+        if(storage.containsKey(diceValue)){
+            storage.put(diceValue, (storage.get(diceValue) + 1));
+        }
+    }
 }
